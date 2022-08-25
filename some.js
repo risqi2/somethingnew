9b438c306fc6dd2d7a94ad5b5661379af9beb388
risqi <risqiikhsani16@gmail.com>
2022-08-25 20:47:21 +0700

    updated some.js add f and g

--- some.js
+++ some.js
@@ -6,6 +6,8 @@ const b = "whatsupp"
 const c = "how are you doing"
 const d = "what have you been up to"
 const e = "yo"
-
+const f = "how are you there"
+const g = "go away ! "
+ 
 
 console.log(a)
